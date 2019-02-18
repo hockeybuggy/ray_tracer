a21c78c4008749aa44a3b8c8f720e7b7274a9d51
Douglas Anderson <hockeybuggy@gmail.com>
2019-02-18 09:12:53 -0500

    Create a `Color` type.
    I was really tempted to create a `Colour` type instead, but alas.

--- src/main.rs
+++ src/main.rs
@@ -123,8 +123,67 @@ fn cross(a: &Tuple, b: &Tuple) -> Tuple {
     );
 }
 
+#[derive(Debug, PartialEq)]
+struct Color {
+    r: f64,
+    g: f64,
+    b: f64,
+}
+
+fn color(r: f64, g: f64, b: f64) -> Color {
+    Color { r, g, b }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Color {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b,
+        }
+    }
+}
+
+impl Sub for Color {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Color {
+            r: self.r - other.r,
+            g: self.g - other.g,
+            b: self.b - other.b,
+        }
+    }
+}
+
+impl Mul<f64> for Color {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        Color {
+            r: self.r * other,
+            g: self.g * other,
+            b: self.b * other,
+        }
+    }
+}
+
+impl Mul for Color {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Color {
+            r: self.r * other.r,
+            g: self.g * other.g,
+            b: self.b * other.b,
+        }
+    }
+}
+
 #[cfg(test)]
-mod tests {
+mod tuple_tests {
     use super::*;
 
     #[test]
@@ -351,5 +410,54 @@ mod tests {
         let vector2 = vector(2.0, 3.0, 4.0);
         assert_eq!(cross(&vector1, &vector2), vector(-1.0, 2.0, -1.0));
         assert_eq!(cross(&vector2, &vector1), vector(1.0, -2.0, 1.0));
+    }
+}
+
+#[cfg(test)]
+mod color_tests {
+    use super::*;
+
+    #[test]
+    fn test_color_constructor() {
+        let color1 = color(-0.5, 0.4, 1.7);
+
+        assert_eq!(color1.r, -0.5);
+        assert_eq!(color1.g, 0.4);
+        assert_eq!(color1.b, 1.7);
+    }
+
+    #[test]
+    fn test_colors_can_be_added() {
+        let color1 = color(0.9, 0.6, 0.75);
+        let color2 = color(0.7, 0.1, 0.25);
+
+        let expected_color = color(1.6, 0.7, 1.0);
+        assert_eq!(color1 + color2, expected_color);
+    }
+
+    #[test]
+    fn test_colors_can_be_subtracted() {
+        let color1 = color(0.9, 0.6, 0.75);
+        let color2 = color(0.7, 0.1, 0.25);
+
+        let expected_color = color(0.9 - 0.7, 0.5, 0.5);
+        assert_eq!(color1 - color2, expected_color);
+    }
+
+    #[test]
+    fn test_colors_can_be_muliplied_by_a_scalar() {
+        let color1 = color(0.2, 0.3, 0.4);
+
+        let expected_color = color(0.4, 0.6, 0.8);
+        assert_eq!(color1 * 2.0, expected_color);
+    }
+
+    #[test]
+    fn test_colors_can_be_muliplied_with_each_other() {
+        let color1 = color(1.0, 0.2, 0.4);
+        let color2 = color(0.9, 1.0, 0.2);
+
+        let expected_color = color(0.9, 0.2, 0.4 * 0.2);
+        assert_eq!(color1 * color2, expected_color);
     }
 }
