868f10b6c00097ecf8e4321cb58e1532ecb24480
Douglas Anderson <hockeybuggy@gmail.com>
2019-08-17 14:41:24 -0400

    Getting started on calculating the normal of a sphere.
    These cases are really simple because it's a unit sphere on the origin.

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,4 +1,5 @@
 use crate::matrix;
+use crate::tuple;
 
 #[derive(Debug, PartialEq)]
 pub struct Sphere {
@@ -9,4 +10,79 @@ pub fn sphere() -> Sphere {
     return Sphere {
         transform: matrix::Matrix4::IDENTITY,
     };
+}
+
+impl Sphere {
+    fn normal_at(&self, point: tuple::Tuple) -> tuple::Tuple {
+        point
+    }
+}
+
+#[cfg(test)]
+mod sphere_tests {
+    // use assert_approx_eq::assert_approx_eq;
+
+    use crate::sphere;
+    use crate::tuple;
+
+    #[test]
+    fn test_normal_on_a_sphere_on_the_x() {
+        let sphere = sphere::sphere();
+
+        let normal = sphere.normal_at(tuple::point(1.0, 0.0, 0.0));
+
+        let expected = tuple::point(1.0, 0.0, 0.0);
+        assert_eq!(expected, normal);
+    }
+
+    #[test]
+    fn test_normal_on_a_sphere_on_the_y() {
+        let sphere = sphere::sphere();
+
+        let normal = sphere.normal_at(tuple::point(0.0, 1.0, 0.0));
+
+        let expected = tuple::point(0.0, 1.0, 0.0);
+        assert_eq!(expected, normal);
+    }
+
+    #[test]
+    fn test_normal_on_a_sphere_on_the_z() {
+        let sphere = sphere::sphere();
+
+        let normal = sphere.normal_at(tuple::point(0.0, 0.0, 1.0));
+
+        let expected = tuple::point(0.0, 0.0, 1.0);
+        assert_eq!(expected, normal);
+    }
+
+    #[test]
+    fn test_normal_on_a_sphere_at_a_nonaxial_point() {
+        let sphere = sphere::sphere();
+
+        let normal = sphere.normal_at(tuple::point(
+            3.0_f64.sqrt() / 3.0,
+            3.0_f64.sqrt() / 3.0,
+            3.0_f64.sqrt() / 3.0,
+        ));
+
+        let expected = tuple::point(
+            3.0_f64.sqrt() / 3.0,
+            3.0_f64.sqrt() / 3.0,
+            3.0_f64.sqrt() / 3.0,
+        );
+        assert_eq!(expected, normal);
+    }
+
+    #[test]
+    fn test_normal_is_mornalized_vector() {
+        let sphere = sphere::sphere();
+
+        let normal = sphere.normal_at(tuple::point(
+            3.0_f64.sqrt() / 3.0,
+            3.0_f64.sqrt() / 3.0,
+            3.0_f64.sqrt() / 3.0,
+        ));
+
+        assert_eq!(tuple::normalize(&normal), normal);
+    }
 }
