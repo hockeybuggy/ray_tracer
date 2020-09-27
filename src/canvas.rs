6f293628d985e80316d8f8b4171adf44b047926a
Douglas Anderson <hockeybuggy@gmail.com>
2020-09-27 09:27:38 -0400

    Add a `canvas_to_image` function
    This function implemented on the Canvas struct returns an rgb image from the
    `image` crate. This crate should allow writing different formats other
    than the ppm format.
    
    My plan is to remove the ppms and replace them with a different image
    format (maybe png?) and change the tests to write fewer things to disk.
    My theory is that this will make the tests run faster. Right now it
    takes 18 seconds to run them all.

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,5 +1,6 @@
 use crate::color;
 
+use image::{ImageBuffer, RgbImage};
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
@@ -33,6 +34,26 @@ impl Canvas {
 
     pub fn pixel_at(&self, x: u32, y: u32) -> &color::Color {
         &self.grid[y as usize][x as usize]
+    }
+
+    pub fn canvas_to_image(&self) -> RgbImage {
+        let mut img: RgbImage = ImageBuffer::new(self.width, self.height);
+
+        let clamp = |x: f64| (255.0_f64.min(x * 255.0).max(0.0) as u8);
+        let (width, height) = img.dimensions();
+        for x in 0..width {
+            for y in 0..height {
+                let canvas_color = self.pixel_at(x, y);
+                let pixel = image::Rgb([
+                    clamp(canvas_color.r),
+                    clamp(canvas_color.g),
+                    clamp(canvas_color.b),
+                ]);
+                img.put_pixel(x, y, pixel);
+            }
+        }
+
+        return img;
     }
 
     pub fn canvas_to_ppm(&self, out_f: &mut File) -> Result<(), std::io::Error> {
@@ -192,5 +213,40 @@ P3
         assert!(contents.contains(expected));
         remove_file(path)?;
         Ok(())
+    }
+
+    #[test]
+    fn test_canvas_to_image_single_color() {
+        let mut canvas1 = canvas::canvas(10, 2);
+        for y in 0..canvas1.height {
+            for x in 0..canvas1.width {
+                canvas1.write_pixel(x, y, color::color(1.0, 0.8, 0.6));
+            }
+        }
+
+        let result = canvas1.canvas_to_image();
+
+        let expected_image =
+            image::ImageBuffer::from_fn(10, 2, |_x, _y| image::Rgb([255, 204, 153]));
+        assert_eq!(expected_image, result);
+    }
+
+    #[test]
+    fn test_canvas_to_image_check_edges_are_included() {
+        let mut canvas1 = canvas::canvas(5, 3);
+        let color1 = color::color(1.5, 0.0, 0.0);
+        canvas1.write_pixel(0, 0, color1);
+        let color2 = color::color(0.0, 0.5, 0.0);
+        canvas1.write_pixel(2, 1, color2);
+        let color3 = color::color(-0.5, 0.0, 1.0);
+        canvas1.write_pixel(4, 2, color3);
+
+        let result = canvas1.canvas_to_image();
+
+        let mut expected_image = image::ImageBuffer::new(5, 3);
+        expected_image.put_pixel(0, 0, image::Rgb([255, 0, 0]));
+        expected_image.put_pixel(2, 1, image::Rgb([0, 127, 0]));
+        expected_image.put_pixel(4, 2, image::Rgb([0, 0, 255]));
+        assert_eq!(expected_image, result);
     }
 }
